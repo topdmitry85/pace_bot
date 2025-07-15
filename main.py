7344e3d3cd7055575758ded140d95b978ed473ee
topdmitry85 <pos-87@mail.ru>
2025-07-15 23:15:10 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ async def debug_file_handler(update: Update, context: ContextTypes.DEFAULT_TYPE)
     await update.message.reply_text("Файл получен! Сейчас попробую его обработать.")
     print("✅ Получен файл:", update.message.document.file_name)
 
-app = ApplicationBuilder().token("ТВОЙ_ТОКЕН").build()
+app = ApplicationBuilder().token("7704340239:AAFFBXNGHOS2pmZgWeF-2icBieGWMkHsPTg").build()
 app.add_handler(MessageHandler(filters.Document.ALL, debug_file_handler))
 app.run_polling()
